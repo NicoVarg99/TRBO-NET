15eddb4924c0f67ec5562237244e051355a04715
Nicola Salsotto <nicolasalsotto@gmail.com>
2019-02-07 22:32:55 +0100

    Use local time in web interface

--- web/state.php
+++ web/state.php
@@ -7,25 +7,25 @@ $state_location = "/var/tmp/arsed_state.json";
 function fetch_state()
 {
     global $state_location;
-    
+
     if (substr($state_location, 0, 1) == '/') {
         $json = file_get_contents($state_location);
     } else {
         // create a new cURL resource
         $ch = curl_init();
-        
+
         // set URL and other appropriate options
         curl_setopt($ch, CURLOPT_URL, $state_location);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        
+
         // grab URL and pass it to the browser
         $json = curl_exec($ch);
-        
+
         // close cURL resource, and free up system resources
         curl_close($ch);
     }
-    
+
     return json_decode($json, true);
 }
 
@@ -36,68 +36,68 @@ function duration_str($s) {
     } else {
         $str = "";
     }
-    
+
     $origs = $s;
-    
+
     if ($s < 1) {
         $str .= "0s";
         return $str;
     }
-    
+
     if ($s >= 24 * 60 * 60) {
         $d = floor($s / (24 * 60 * 60));
         $s -= $d * 24 * 60 * 60;
         $str .= $d . 'd ';
     }
-    
+
     if ($s >= 60 * 60) {
         $d = floor($s / (60 * 60));
         $s -= $d * 60 * 60;
         $str .= $d . "h";
     }
-    
+
     if ($s >= 60) {
         $d = floor($s / 60);
         $s -= $d * 60;
         $str .= $d . "m";
     }
-    
+
     if ($s >= 1) {
         if ($origs < 60*60)
             $str .= floor($s) . "s";
     }
-    
+
     return $str;
 }
 
 
 function logtime($tstamp)
 {
-    return gmstrftime('%Y-%m-%d %T', $tstamp);
+    return strftime('%Y-%m-%d %T', $tstamp);
 }
 
 function lastheard_sort($a, $b)
 {
     if ($a['last_heard'] == $b['last_heard'])
         return 0;
-        
+
     return ($a['last_heard'] > $b['last_heard']) ? -1 : 1;
 }
 
 if ($_GET['u']) {
     #error_log("state update");
-    
+
     header('Content-Type: application/json; charset=utf-8');
-    
+
     $j = array(); # returned json
-    
+
     $state = fetch_state();
     $reg = $state['registry'];
-    
-    $j['at'] = "at " . logtime($state['time']) . " UTC - uptime "
+
+    $j['at'] = "at " . logtime($state['time']) . " " . date('T') . " - uptime "
         . duration_str($state['uptime']);
     $j['radios'] = "Radios: $state[ars_clients_here] registered on network, $state[ars_clients] total";
-    
+
     $here = array();
     $away = array();
     foreach ($reg as $id => $radio) {
@@ -106,13 +106,13 @@ if ($_GET['u']) {
         else
             array_push($away, $radio);
     }
-    
+
     uasort($here, 'lastheard_sort');
     uasort($away, 'lastheard_sort');
-    
+
     $s = "<table>\n"
         . "<tr><th>Callsign</th> <th>ID</th> <th>Registered</th> <th>Last heard</th> <th>Last update</th></tr>\n";
-    
+
     $shown = 0;
     foreach ($here as $radio) {
         $shown++;
@@ -120,7 +120,7 @@ if ($_GET['u']) {
             . "<td>" . (isset($radio['heard_what']) ? $radio['heard_what'] : '') . "</td>"
             . "</tr>\n";
     }
-    
+
     $s .= "<tr class='separator'><td> </td><td> </td><td> </td><td> </td><td> </td></tr>\n";
 
     foreach ($away as $radio) {
@@ -132,16 +132,16 @@ if ($_GET['u']) {
             . "<td>" . (isset($radio['away_reason']) ? $radio['away_reason'] : '') . "</td>"
             . "</tr>\n";
     }
-    
+
     $s .= "</table>\n";
-    
+
     $j['table'] = $s;
-    
+
     $unshown = $state['ars_clients'] - $shown;
     $j['unshown'] = ($unshown) ? "Not showing $unshown configured radios which have never registered." : "";
-    
+
     print json_encode($j);
-    
+
     return;
 } else {
     print_body();
@@ -192,7 +192,7 @@ function refresh()
                     repl("#unshown", data.unshown, function(){});
                 });
             });
-            
+
             setTimeout(function() { refresh(); }, 6000);
         },
         error: function() {
