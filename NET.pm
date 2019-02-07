8c1b3bb1c8c886cd4f9827676eab903fe73725fe
Nicola Salsotto <nicolasalsotto@gmail.com>
2019-02-07 17:26:59 +0100

    Allow custom symbol

--- NET.pm
+++ NET.pm
@@ -37,26 +37,26 @@ Returns new instance of TRBO::NET communicator.
     'debug' => 0,
   );
 
-=back 
+=back
 
 =cut
 
-sub new 
+sub new
 {
     my $class = shift;
     my $self = bless { @_ }, $class;
-    
+
     $self->{'initialized'} = 0;
     $self->{'version'} = $VERSION;
-    
+
     $self->{'ars_clients'} = 0;
     $self->{'ars_clients_here'} = 0;
-    
+
     # store config
     my %h = @_;
     $self->{'config'} = \%h;
     #print "settings: " . Dumper(\%h);
-    
+
     my %defaults = (
         'registry_poll_interval' => 900,
         'registry_timeout' => 1800,
@@ -65,21 +65,21 @@ sub new
         'tms_queue_max_age' => 2*3600,
         'debug' => 0,
     );
-    
+
     foreach my $k (keys %defaults) {
         if (!defined $self->{'config'}->{$k}) {
             $self->{'config'}->{$k} = $defaults{$k}
         }
     }
-    
+
     $self->{'debug'} = ( $self->{'config'}->{'debug'} );
     $self->{'log_prefix'} = $self;
     $self->{'log_prefix'} =~ s/=.*//;
-    
+
     $self->_debug('initializing, config: ' . Dumper($self->{'config'}));
-    
+
     $self->_clear_errors();
-    
+
     $self->{'timeout'} = 10;
     if (defined $h{'timeout'}) {
         if ($h{'timeout'} < 2) {
@@ -90,27 +90,27 @@ sub new
         }
         $self->{'timeout'} = $h{'timeout'}
     }
-    
+
     $self->{'sel'} = IO::Select->new();
-    
+
     foreach my $type ('ars', 'tms', 'loc') {
         my $port = $h{$type . '_port'};
-        
+
         my $s = IO::Socket::INET->new(
             LocalPort => $port,
             Proto => 'udp'
         );
-        
+
         if (!$s) {
             warn "TRBO::NET failed to create $type UDP socket: $!\n";
             return 0;
         }
-        
+
         $self->{$type . '_sock'} = $s;
         $self->{'sel'}->add($s);
         $self->_debug("listening on $type port $port");
     }
-    
+
     $self->{'ars'} = TRBO::ARS->new(
         'debug' => ($h{'debug'}),
         'sock' => $self->{'ars_sock'},
@@ -133,14 +133,14 @@ sub new
         'tms_queue_max_age' => $h{'tms_queue_max_age'},
     );
     $self->{'tms'}->init();
-    
+
     # registry of radios currently followed
     $self->{'registry'} = {};
     $self->{'reg_call'} = {};
-    
+
     $self->{'pkts_rx'} = 0;
     $self->{'bytes_rx'} = 0;
-    
+
     return $self;
 }
 
@@ -158,7 +158,7 @@ to registry item.
 sub add_radio($$)
 {
     my($self, $id) = @_;
-    
+
     my %h = (
         'id' => $id,
         'first_heard' => 0,
@@ -167,11 +167,11 @@ sub add_radio($$)
         'last_poll_rx' => 0,
         'state' => 'away',
     );
-    
+
     $self->{'registry'}->{$id} = \%h;
-    
+
     $self->{'ars_clients'} += 1;
-    
+
     return $self->{'registry'}->{$id};
 }
 
@@ -188,14 +188,16 @@ Configure radio with callsign, and add to registry.
 sub configure_radio($$)
 {
     my($self, $rx) = @_;
-    
+
     $self->_debug("configuring radio " . $rx->{'id'} . ": " . $rx->{'callsign'});
-    
+
     my $radio = $self->add_radio($rx->{'id'});
-    
+
     $radio->{'callsign'} = $rx->{'callsign'};
     $self->{'reg_call'}->{$rx->{'callsign'}} = $radio;
-    
+
+    $radio->{'symbol'} = $rx->{'symbol'};
+
     return $radio;
 }
 
@@ -213,23 +215,23 @@ currently registered on network.
 sub register_radio($$)
 {
     my($self, $rx) = @_;
-    
+
     $self->_debug("registering radio " . $rx->{'src_id'});
     $self->_info($rx->{'src_id'} . ": Registering on net");
-    
+
     my $radio;
     if (!defined $self->{'registry'}->{$rx->{'src_id'}}) {
         $radio = $self->add_radio($rx->{'src_id'});
     } else {
         $radio = $self->{'registry'}->{$rx->{'src_id'}};
     }
-    
+
     $radio->{'first_heard'}
         = $radio->{'last_heard'}
         = $radio->{'last_poll_tx'}
         = $radio->{'last_poll_rx'}
         = time();
-    
+
     $self->_registry_here($rx->{'src_id'});
 }
 
@@ -246,16 +248,16 @@ Find radio from registry by callsign.
 sub registry_find_call($$)
 {
     my($self, $call) = @_;
-    
+
     return if (!defined $self->{'reg_call'}->{$call});
-    
+
     return $self->{'reg_call'}->{$call};
 }
 
 sub _registry_heard($$)
 {
     my($self, $id) = @_;
-    
+
     if (defined $self->{'registry'}->{$id}) {
         $self->_debug("registry updating last_heard: $id");
         $self->{'registry'}->{$id}->{'last_heard'} = time();
@@ -264,7 +266,7 @@ sub _registry_heard($$)
         }
         return;
     }
-    
+
     # we received packet from radio we don't know about.
     # How to tell radio to give us new Hello!
 }
@@ -272,7 +274,7 @@ sub _registry_heard($$)
 sub _registry_pong($$)
 {
     my($self, $id) = @_;
-    
+
     if (defined $self->{'registry'}->{$id}) {
         $self->_debug("registry updating last_poll_rx: $id");
         $self->{'registry'}->{$id}->{'last_poll_rx'} = time();
@@ -286,7 +288,7 @@ sub _registry_here($$)
     if (!defined $self->{'registry'}->{$id}) {
         return;
     }
-    
+
     if ($self->{'registry'}->{$id}->{'state'} ne 'here') {
         $self->{'ars_clients_here'} += 1;
         $self->_info($id . ": Marking here");
@@ -300,7 +302,7 @@ sub _registry_last($$$)
     if (!defined $self->{'registry'}->{$id}) {
         return;
     }
-    
+
     $self->{'registry'}->{$id}->{'heard_what'} = $reason;
 }
 
@@ -310,11 +312,11 @@ sub _registry_leave($$$)
     if (!defined $self->{'registry'}->{$id}) {
         return;
     }
-    
+
     if ($self->{'registry'}->{$id}->{'state'} eq 'here') {
         $self->{'ars_clients_here'} -= 1;
     }
-    
+
     $self->{'registry'}->{$id}->{'state'} = 'away';
     $self->{'registry'}->{$id}->{'away_reason'} = $reason;
     $self->_info($id . ": Marking away ($reason)");
@@ -326,7 +328,7 @@ sub _registry_get($$)
     if (!defined $self->{'registry'}->{$id}) {
         return;
     }
-    
+
     return $self->{'registry'}->{$id};
 }
 
@@ -344,10 +346,10 @@ radios as necessary.
 sub registry_scan($;$)
 {
     my($self, $noping) = @_;
-    
+
     my $now = time();
     $self->{'ars_clients_here'} = 0;
-    
+
     foreach my $id (keys %{ $self->{'registry'} }) {
         my $r = $self->{'registry'}->{$id};
         #print Dumper($r);
@@ -383,59 +385,59 @@ Receive UDP packets on sockets and pass them to other modules.
 sub receive($)
 {
     my($self) = @_;
-    
+
     # wait for UDP packets on any of sockets
     my @ready = $self->{'sel'}->can_read(1);
-    
+
     if (!@ready) {
         #$self->_debug("select timed out");
         return;
     }
-    
+
     # only read from first one, for now
     my $fh = shift @ready;
-    
+
     my $msg;
     my $remote_address = recv($fh, $msg, 1500, 0);
-    
+
     $self->{'pkts_rx'} +=  1;
     $self->{'bytes_rx'} += length($msg);
-    
+
     if (!defined $remote_address) {
         $self->_debug("recv on socket failed");
         return;
     }
-    
+
     my ($peer_port, $peer_addr) = unpack_sockaddr_in($remote_address);
     my $addr_s = inet_ntoa($peer_addr);
-    
+
     $self->_debug("received from $addr_s: $peer_port: " . length($msg) . " bytes:");
     $self->_debug(TRBO::Common::_hex_dump($msg));
-    
+
     # decode any information available in IP header
     my %h = (
         'src_ip' => $addr_s,
         'src_port' => $peer_port,
         'msg' => 'unknown',
     );
-    
+
     my @t = split(/\./, $addr_s);
     $h{'src_cai'} = $t[0] * 1;
     $h{'src_id'} = $t[3] + $t[2]*256 + $t[1]*256*256;
-    
+
     if ($h{'src_cai'} != $self->{'config'}->{'cai_net'}) {
         $self->_fail(\%h, 'invalid_cai');
         return \%h;
     }
-    
+
     $self->_registry_heard($h{'src_id'});
-    
+
     $h{'registry'} = $self->_registry_get($h{'src_id'});
-    
+
     # multiplex to protocol handlers
     if ($peer_port eq $self->{'config'}->{'ars_port'}) {
         my $ret = $self->{'ars'}->decode(\%h, $msg);
-        
+
         if ($h{'msg'} eq 'hello') {
             $self->{'ars'}->ack_hello($h{'src_id'});
             $self->_registry_last($h{'src_id'}, 'hello'); # mark it as being present, too
@@ -445,7 +447,7 @@ sub receive($)
         } elsif ($h{'msg'} eq 'leave') {
             $self->_registry_leave($h{'src_id'}, 'leave');
         }
-        
+
     } elsif ($peer_port eq $self->{'config'}->{'loc_port'}) {
         $self->_registry_here($h{'src_id'}); # mark it as being present, too
         $self->{'loc'}->decode(\%h, $msg);
@@ -458,9 +460,8 @@ sub receive($)
             $self->_registry_last($h{'src_id'}, 'tms msg in'); # mark it as being present, too
         }
     }
-    
+
     return \%h;
 }
 
 1;
-
